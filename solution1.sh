6c87b68a768cdd36afc96b859d833fa863e3bbb8
ABOGOL2020 <91035163+ABOGOL2020@users.noreply.github.com>
2022-12-11 20:08:11 -0600

    Update solution1.sh

--- solution1.sh
+++ solution1.sh
@@ -5,9 +5,13 @@
 # help command baked in to the cli.
 terraform version
 ####
+mkdir terraform
 git clone https://github.com/ABOGOL2020/my_terraform_project.git
 create a new folder "project_1"
 copy mike_web_app to project_1
+
+#####
+cd terraform/project_1/mike_web_app
 
 # Terraform
 terraform init
